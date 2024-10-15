48fe001503e1acdecd2e73e3ac7c342fbf5f9cec
saraisa12 <saraisa.024@gmail.com>
2024-10-15 18:32:02 +0300

    done with multer

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose')
-const express = require('express')
-const cors = require('cors')
+const mongoose = require("mongoose")
+const express = require("express")
+const cors = require("cors")
 
 // Load environment variables from .env file
-require('dotenv').config()
+require("dotenv").config()
 
 // Load DB
-require('./config/db')
+require("./config/db")
 
 // PORT Configurations
 const PORT = process.env.PORT || 4000
@@ -20,16 +20,15 @@ app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 // Import routes
-const eventRouter = require('./routes/event')
-const authRouter = require('./routes/AuthRouter') // Import your AuthRouter
+const eventRouter = require("./routes/event")
+const authRouter = require("./routes/AuthRouter") // Import your AuthRouter
 const reservationsRouter = require("./routes/reservations")
 
 //mount routes
-
+app.use("/uploads", express.static("uploads"))
 app.use("/event", eventRouter)
 app.use("/reservations", reservationsRouter)
-app.use('/auth', authRouter) // Mount the AuthRouter for authentication
-
+app.use("/auth", authRouter) // Mount the AuthRouter for authentication
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`)
